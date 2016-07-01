257747fd07c21f74644300213342692643e51ea5
bagofmice <bagofmice@gmail.com>
2016-07-01 00:12:55 -0700

    path

--- AndroidSetup.ps1
+++ AndroidSetup.ps1
@@ -13,7 +13,7 @@ XCOPY /E /Y "C:\Program Files (x86)\Android\android-sdk-windows"  $Env:ANDROID_H
 Start-BitsTransfer -Source "https://github.com/bagonaut/Scripts/raw/master/pressy.exe" -Destination "C:\prereq\pressy.exe"
 #Environment vars in machine for Future use
 [Environment]::SetEnvironmentVariable("ANDROID_HOME", $Env:Android_Home, [System.EnvironmentVariableTarget]::Machine)
-$PathVar = [System.String]::concat("$Env:Path;", "$Env:Android_Home", "\Tools;", "$Env:ANDROID_HOME", "\platform-tools;") 
+$PathVar = [System.String]::concat("$Env:Path;", "$Env:Android_Home", "Tools;", "$Env:ANDROID_HOME", "platform-tools;") 
 $Env:Path = $PathVar 
 Write-Output "Setting Path Env"
 [Environment]::SetEnvironmentVariable("Path", $Env:Path, [System.EnvironmentVariableTarget]::Machine)
