80be95eafe73f5424b364b6f44621140b14ffc3f
bagofmice <bagofmice@gmail.com>
2016-07-01 11:29:48 -0700

    Adding pressy

---
+++ pressy/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string targetApp = "java";
+            for (int i = 0; i < 100; i++)
+            {
+                try // worth a try
+                {
+                    Microsoft.VisualBasic.Interaction.AppActivate(targetApp);
+                }
+                catch (System.ArgumentException)
+                {
+                    int x = 1;
+                }
+                
+                SendKeys.SendWait("y"); // press y anyways
+                SendKeys.SendWait("~");
+                System.Threading.Thread.Sleep(1000);
+            }
+            System.Threading.Thread.CurrentThread.Abort();
+        }
+    }
+}
