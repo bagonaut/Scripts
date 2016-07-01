e58542fbb009a98a98c9d9f2a6161e57b1e1d3a4
bagofmice <bagofmice@gmail.com>
2016-07-01 11:31:31 -0700

    Adding source for pressy

--- AndroidSetup.ps1
+++ AndroidSetup.ps1
@@ -9,7 +9,7 @@ if (-Not [System.IO.Directory]::Exists($Env:ANDROID_HOME)) {Write-output "Making
 [System.IO.Compression.ZipFile]::ExtractToDirectory($AndroidZip, $Android_Root)
 #Rename to match Visual Studio
 XCOPY /E /Y "C:\Program Files (x86)\Android\android-sdk-windows"  $Env:ANDROID_HOME
-#download Eula Acceptor
+#download Eula Acceptor Get source code here: https://github.com/bagonaut/Scripts/tree/master/pressy
 Start-BitsTransfer -Source "https://github.com/bagonaut/Scripts/raw/master/pressy.exe" -Destination "C:\prereq\pressy.exe"
 #Environment vars in machine for Future use
 [Environment]::SetEnvironmentVariable("ANDROID_HOME", $Env:Android_Home, [System.EnvironmentVariableTarget]::Machine)
@@ -20,7 +20,6 @@ Write-Output "Setting Path Env"
 write-output $env:PATH
 #Eula Acceptor
 new-alias -name y -value "out-null" -Force -Scope Global #squelch extra y
-get-alias y
 y
 C:\prereq\pressy.exe # Accept Eulas
 get-process pressy
@@ -28,6 +27,6 @@ $updateCmd = [System.IO.Path]::Combine($Env:ANDROID_HOME, "tools\android.bat")
 $updateArgs = "update sdk -u"
 cd ([System.IO.Path]::Combine($Env:ANDROID_HOME, "tools")) #Your java treachery knows no bounds
 Invoke-Expression " & '$updateCmd' $updateArgs"
-sleep(100) #ensure this script does not return until y stops
-get-alias y
+
+
 
